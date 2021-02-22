311e5867f7d74c13245477d9bc0b04c60a509cdd
Hari Sekhon <harisekhon@gmail.com>
2021-02-22 10:45:07 +0000

    updated iam.tf

--- iam.tf
+++ iam.tf
@@ -17,35 +17,29 @@
 
 # =======
 # GCP IAM
+#
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+
 resource "google_project_iam_member" "project-editors" {
-  #project = "your-project-id"
 
   # to find the role id which doesn't quite correspond to the human names in GCP Console UI, run this:
   #
   #   gcloud projects get-iam-policy <myproject>
   #
-  role    = "roles/editor"
+  role = "roles/editor"
 
   # XXX: edit this - don't put individuals users in here please, it's hard to maintain
-  member  = [
-    "group:admins@mydomain.com"
-  ]
+  member = "group:admins@mydomain.com"
 }
 
 resource "google_project_iam_member" "cloudsql-viewer" {
-  #project = "your-project-id"
-  role    = "roles/cloudsql.viewer"
+  role = "roles/cloudsql.viewer"
   # XXX: edit this
-  member  = [
-    "serviceAccount:cloud-function-sql-backup@<myproject>.iam.gserviceaccount.com"
-  ]
+  member = "serviceAccount:cloud-function-sql-backup@<myproject>.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cloudsql-client" {
-  #project = "your-project-id"
-  role    = "roles/cloudsql.client"
+  role = "roles/cloudsql.client"
   # XXX: edit this
-  member  = [
-    "serviceAccount:cloud-function-sql-backup@<myproject>.iam.gserviceaccount.com"
-  ]
+  member = "serviceAccount:cloud-function-sql-backup@<myproject>.iam.gserviceaccount.com"
 }
