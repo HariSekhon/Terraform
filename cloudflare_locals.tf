26e511e366ffdc231b94d23416ff60502a3089a6
Hari Sekhon <harisekhon@gmail.com>
2021-03-22 20:28:09 +0000

    added cloudflare_locals.tf

---
+++ cloudflare_locals.tf
@@ -0,0 +1,30 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-03-04 18:19:03 +0000 (Thu, 04 Mar 2021)
+#
+#  https://github.com/HariSekhon/terraform
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#              C l o u d f l a r e   L o c a l   V a r i a b l e s
+# ============================================================================ #
+
+# required by cloudflare_firewall*.tf
+
+locals {
+
+  # obtained from the Cloudflare API:
+  #
+  #   cloudflare_zones.sh
+  #
+  # XXX: Edit
+  cloudflare_zone_id = "..."
+
+}
