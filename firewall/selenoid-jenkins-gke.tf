9bd3777c3eb64d5d7b5c09480ecd29e5b86f2de8
Hari Sekhon <harisekhon@gmail.com>
2021-03-29 15:13:25 +0100

    updated selenoid-jenkins-gke.tf

--- firewall/selenoid-jenkins-gke.tf
+++ firewall/selenoid-jenkins-gke.tf
@@ -18,7 +18,7 @@
 
 resource "google_compute_firewall" "selenoid-hub-gke" {
   name        = "selenoid-hub-gke-${basename(module.vpc.vpc_network)}"
-  network = basename(module.vpc.vpc_network)
+  network     = basename(module.vpc.vpc_network)
   description = "Selenoid Hub access from GKE VMs"
 
   allow {
@@ -26,8 +26,9 @@ resource "google_compute_firewall" "selenoid-hub-gke" {
     ports    = ["4444"]
   }
 
-  source_tags = ["goog-gke-node"]  # Jenkins run in GKE
-  target_tags = ["selenoid-deployment"]  # label we've applied to marketplace vm
+  #source_tags = ["goog-gke-node"]  # Jenkins runs in GKE, but you cannot edit network labels for a node pool, so this would have to have been set at creation time. The goog-gke-node default cluster label cannot be used as a network tag
+  source_ranges = [local.Cloud_Nat_IP]    # IP address of the Cloud NAT that GKE uses to go externally
+  target_tags   = ["selenoid-deployment"] # label I've applied to Marketplace VM
 }
 
 resource "google_compute_firewall" "selenoid-ui-vpn" {
