fae98f5569c8fd5df93caf570f167c60c31f9084
Hari Sekhon <harisekhon@gmail.com>
2021-05-12 16:58:41 +0100

    added provider.tf

---
+++ cloudflare/firewall/provider.tf
@@ -0,0 +1,34 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-05-12 16:55:25 +0100 (Wed, 12 May 2021)
+#
+#  https://github.com/HariSekhon/Terraform-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                              C l o u d f l a r e
+# ============================================================================ #
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+# in terraform.tfvars
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+}
+
+provider "http" {}
