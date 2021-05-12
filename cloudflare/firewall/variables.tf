2003334c4ea6862eba60568c8fdaf164d3b47a36
Hari Sekhon <harisekhon@gmail.com>
2021-05-12 16:58:32 +0100

    updated variables.tf

--- cloudflare/firewall/variables.tf
+++ cloudflare/firewall/variables.tf
@@ -1,3 +1,21 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: 2021-05-12 16:55:25 +0100 (Wed, 12 May 2021)
+#
+#  https://github.com/HariSekhon/Terraform-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                              C l o u d f l a r e
+# ============================================================================ #
+
 variable "cloudflare_email" {
   type = string
 }
