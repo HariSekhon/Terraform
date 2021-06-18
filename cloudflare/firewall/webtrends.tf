6a10522761ff62afcd196a1957585605e1ad3fc8
Hari Sekhon <harisekhon@gmail.com>
2021-06-18 16:46:57 +0100

    updated webtrends.tf

--- cloudflare/firewall/webtrends.tf
+++ cloudflare/firewall/webtrends.tf
@@ -13,25 +13,27 @@
 #
 
 # ============================================================================ #
-#         C l o u d f l a r e   F i r e w a l l   -   W e b t r e n d s
+#        Cloudflare Firewall - Webtrends Optimize (not Webtrends proper)
 # ============================================================================ #
 
+# Webtrends Optimize was sold by Webtrends to a smaller company - so this is not Webtrends source IPs
+
 locals {
-  webtrends_IPs = [
+  webtrends_optimize_IPs = [
     "213.123.137.249",
     "62.31.105.234"
   ]
 }
 
-resource "cloudflare_filter" "webtrends" {
+resource "cloudflare_filter" "webtrends-optimize" {
   zone_id     = var.zone_id
-  description = "Webtrends IPs"
-  expression  = "( ip.src in { ${join("\n", local.webtrends_IPs)} } )"
+  description = "Webtrends Optimize IPs"
+  expression  = "( ip.src in { ${join("\n", local.webtrends_optimize_IPs)} } )"
 }
 
-resource "cloudflare_firewall_rule" "webtrends" {
+resource "cloudflare_firewall_rule" "webtrends-optimize" {
   zone_id     = var.zone_id
-  description = "Webtrends"
-  filter_id   = cloudflare_filter.webtrends.id
+  description = "Webtrends Optimize"
+  filter_id   = cloudflare_filter.webtrends-optimize.id
   action      = "allow"
 }
