c205012ffc020541220128c2980de65e70166b2a
Hari Sekhon <harisekhon@gmail.com>
2021-07-21 19:05:09 +0100

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -13,16 +13,34 @@
 #
 
 terraform {
+
+  # ============================
+  # Newbies get this by default:
+  #
+  #default backend
+  #backend "local" {
+  #  path = "./terraform.tfstate"
+  #}
+
+  # ===========
+  # Cloud Pros:
+  #
+  # =============
+  # Google Cloud:
+  #
   # XXX: remember to enable Object Versioning on this GCS bucket for state recovery:
   #
   #		https://cloud.google.com/storage/docs/object-versioning
   #
   #   gsutil versioning set on gs://$GOOGLE_PROJECT_ID-productiontf-state
-  backend "gcs" {
-    bucket = "NAME-production-tf-state"   # XXX: EDIT
-    prefix = "terraform/state/production" # <prefix>/<name>.tfstate
-  }
+  #backend "gcs" {
+  #  bucket = "NAME-production-tf-state"   # XXX: EDIT
+  #  prefix = "terraform/state/production" # <prefix>/<name>.tfstate
+  #}
 
+  # ====================
+  # Amazon Web Services:
+  #
   # XXX: remember to enable Bucket Versioning on this S3 bucket for state recovery:
   #
   #		https://docs.aws.amazon.com/AmazonS3/latest/user-guide/enable-versioning.html
@@ -31,33 +49,30 @@ terraform {
   #
   #		https://www.terraform.io/docs/backends/types/s3.html#s3-bucket-permissions
   #
-  #  backend "s3" {
-  #    bucket = "mybucket" # XXX: EDIT
-  #    key    = "path/to/my/key"
-  #    region = "eu-west-1"
-  #    # XXX: EDIT - set DynamoDB table name and configure for locking:
-  #    #
-  #    #  https://www.terraform.io/docs/backends/types/s3.html#dynamodb-state-locking
-  #    #
-  #    dynamodb_table = "mytable"
-  #  }
+  #backend "s3" {
+  #  bucket = "mybucket" # XXX: EDIT
+  #  key    = "path/to/my/key"
+  #  region = "eu-west-1"
+  #  # XXX: EDIT - set DynamoDB table name and configure for locking:
+  #  #
+  #  #  https://www.terraform.io/docs/backends/types/s3.html#dynamodb-state-locking
+  #  #
+  #  dynamodb_table = "mytable"
+  #}
 
-  # default backend
-  #  backend "local" {
-  #    path = "./terraform.tfstate"
-  #  }
-
-  # Terraform Cloud
-  #  backend "remote" {
-  #    hostname = "app.terraform.io"
-  #    organization = "mycompany"  # XXX: EDIT
+  # ================
+  # Terraform Cloud:
   #
-  #    workspaces {
-  #      # XXX: EDIT
-  #      # single workspace
-  #      name = "my-app-prod"
-  #      # or multiple workspaces
-  #      prefix = "my-app-"
-  #    }
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "mycompany"  # XXX: EDIT
+  #
+  #  workspaces {
+  #    # XXX: EDIT
+  #    # single workspace
+  #    name = "my-app-prod"
+  #    # or multiple workspaces
+  #    prefix = "my-app-"
   #  }
+  #}
 }
