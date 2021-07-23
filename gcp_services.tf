9e6c8d1294677f4332bbffa0ad55c801dec9d5e5
Hari Sekhon <harisekhon@gmail.com>
2021-07-23 10:32:11 +0100

    added gcp_services.tf

---
+++ gcp_services.tf
@@ -0,0 +1,36 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: [% DATE # 2021-07-23 10:32:00 +0100 (Fri, 23 Jul 2021) %]
+#
+#  [% URL %]
+#
+#  [% LICENSE %]
+#
+#  [% MESSAGE %]
+#
+#  [% LINKEDIN %]
+#
+
+# ============================================================================ #
+#      T e r r a f o r m   -   G C P   S e r v i c e s   t o   E n a b l e
+# ============================================================================ #
+
+# example of automating the Cloud Functions backup with Terraform
+#
+# https://medium.com/better-programming/how-to-automate-google-cloud-sql-backups-2de6d3cc7d01
+
+locals {
+  # GCP enable these services
+  services_to_enable = [
+    "cloudscheduler.googleapis.com",
+    "sqladmin.googleapis.com",
+    "cloudfunctions.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "service" {
+  for_each = toset(local.services_to_enable)
+  project  = var.project_id
+  service  = each.value
+}
