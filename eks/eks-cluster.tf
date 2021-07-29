6f9c85028664c9853f7ade065ea5dd6e572a14d1
Hari Sekhon <harisekhon@gmail.com>
2021-07-29 17:11:48 +0100

    added eks module

---
+++ eks/eks-cluster.tf
@@ -0,0 +1,30 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.21"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "r5.large"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      #additional_userdata           = "echo foo bar"
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
---
+++ eks/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
