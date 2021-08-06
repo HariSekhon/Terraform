b224cd6c1b53bf10fbb65a082dc23a8f140966d4
Hari Sekhon <harisekhon@gmail.com>
2021-08-06 11:28:11 +0100

    updated bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -13,6 +13,36 @@
 #
 
 # ============================================================================ #
+#                             A W S   B u c k e t s
+# ============================================================================ #
+
+                         # XXX: Edit
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "${var.project_name}-mybucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true
+    #mfa_delete = true  # default: false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
+}
+
+
+# ============================================================================ #
 #                             G C P   B u c k e t s
 # ============================================================================ #
 
@@ -20,6 +50,7 @@
 resource "google_storage_bucket" "mybucket" {
                                                  # XXX: edit
   name                        = "${var.project_id}-mybucket"
+  #name                        = "${var.project_name}-mybucket"
   location                    = "EU"
   uniform_bucket_level_access = true # XXX: GCS defaults to fine-grained security otherwise, which is more likely to have a human misconfiguration data leak
 }
