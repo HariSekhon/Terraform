5582bb195c9e5384eedfd5a5768b155056703f3c
Hari Sekhon <harisekhon@gmail.com>
2021-10-12 16:15:15 +0100

    updated eks-cluster.tf

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,3 +1,5 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
