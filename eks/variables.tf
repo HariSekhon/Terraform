34d1e4e33b2d01532fe46c812798603904611045
Hari Sekhon <harisekhon@gmail.com>
2021-11-25 16:33:00 +0000

    updated variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -5,3 +5,9 @@ variable "cluster_name" {
 variable "region" {
   type = string
 }
+
+# to enable users/groups other than the cluster creator owner to access it
+variable "map_roles" {
+  type    = list(object({ rolearn = string, username = string, groups = list(string) }))
+  default = []
+}
