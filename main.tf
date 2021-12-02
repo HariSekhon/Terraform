fa5afcb48156c8978d0534c31b292f5a1e9128df
Hari Sekhon <harisekhon@gmail.com>
2021-12-02 18:42:50 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,16 @@ module "NAME" {
   region  = local.region
   network = module.vpc.vpc_network
 }
+
+module "eks" {
+  source       = "../modules/eks"
+  region       = local.region
+  cluster_name = "mycluster"
+  map_roles = [
+    {
+      rolearn  = "arn:aws:iam::123456789012:role/AWSReservedSSO_MyGroup_1234a567b890cdef",
+      username = "myrole"
+      groups   = []
+    }
+  ]
+}
