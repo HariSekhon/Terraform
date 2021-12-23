96f7b58b1f37c7b5df836eb157948e835bee1595
Hari Sekhon <harisekhon@gmail.com>
2021-12-23 20:34:23 +0000

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,7 +17,7 @@
 # ============================================================================ #
 
 output "db-password" {
-  value = aws_db_instance.db.password
+  value       = aws_db_instance.db.password
   description = "Password for database"
-  sensitive = true  # won't be output in Terminal but will be in state file
+  sensitive   = true # won't be output in Terminal but will be in state file
 }
