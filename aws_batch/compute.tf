42773350d0f1d6ff7ca1642c144e1147d07f5ac5
Hari Sekhon <harisekhon@gmail.com>
2022-01-12 15:54:58 +0000

    updated compute.tf

--- aws_batch/compute.tf
+++ aws_batch/compute.tf
@@ -6,10 +6,10 @@ resource "aws_batch_compute_environment" "mycompute" {
   compute_resources {
     type                = "SPOT"
     allocation_strategy = "SPOT_CAPACITY_OPTIMIZED"
-    bid_percentage      = 100
+    bid_percentage      = 50
     instance_type       = ["optimal"]
     min_vcpus           = 0
-    max_vcpus           = 12000  # XXX: Edit
+    max_vcpus           = 4750  # XXX: Edit
     subnets             = var.vpc_subnet_ids
     security_group_ids  = var.vpc_security_group_ids
     instance_role       = aws_iam_instance_profile.ecs_instance_role.arn
