558a12691e7cde802f9a136163b090f506f304c6
Hari Sekhon <harisekhon@gmail.com>
2022-02-25 19:44:41 +0000

    updated github_actions_org_permissions.tf

--- github_actions_org_permissions.tf
+++ github_actions_org_permissions.tf
@@ -20,12 +20,12 @@
 
 # name is just for reference purposes, the org will be set in github{} in provider.tf
 resource "github_actions_organization_permissions" "MYORG" {
-  allowed_actions = "selected"
-  enabled_repositories = "all"  # if "selected" then specify list of allowed repos below
+  allowed_actions      = "selected"
+  enabled_repositories = "all" # if "selected" then specify list of allowed repos below
   allowed_actions_config {
     github_owned_allowed = true
     verified_allowed     = true
-    patterns_allowed     = [
+    patterns_allowed = [
       "aquasecurity/trivy-action@*",
       "dawidd6/action-download-artifact@c776311571668da4ded92c1376aa799424360b2d",
       "peaceiris/actions-gh-pages@068dc23d9710f1ba62e86896f84735d869951305",
