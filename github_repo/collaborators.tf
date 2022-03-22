0266e68683ead88fbc9163e2fb659a738aad3d9f
Hari Sekhon <harisekhon@gmail.com>
2022-03-22 16:42:04 +0000

    added collaborators.tf

---
+++ github_repo/collaborators.tf
@@ -0,0 +1,23 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: 2022-03-01 12:00:27 +0000 (Tue, 01 Mar 2022)
+#
+#  https://github.com/HariSekhon/Terraform
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#                            Individual Collaborators
+# ============================================================================ #
+
+resource "github_repository_collaborator" "ci" {
+  repository = github_repository.repo.id
+  username   = "my-ci-machine-user"
+  permission = "pull"
+}
