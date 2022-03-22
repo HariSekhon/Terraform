e8f70c0d304919bb949b77b580fe2ce1eac49552
Hari Sekhon <harisekhon@gmail.com>
2022-03-22 18:27:47 +0000

    updated codeowners.tf

--- github_repo/codeowners.tf
+++ github_repo/codeowners.tf
@@ -27,7 +27,7 @@ resource "github_repository_file" "codeowners" {
   # requires both or neither - uses the account owning the github token as the author if omitted
   #commit_author = "Terraform"
   #commit_email  = "terraform@MYCOMPANY.COM"
-  overwrite_on_create = true
+  overwrite_on_create = false
 
   lifecycle {
     ignore_changes = [
