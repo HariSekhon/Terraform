2cb1ae03f790bb26e5c6f8e50df115516a1310c4
Hari Sekhon <harisekhon@gmail.com>
2022-03-22 18:43:44 +0000

    Terraform Fmt fixes - GitHub Actions Workflow: Fmt

--- github_collaborator.tf
+++ github_collaborator.tf
@@ -20,7 +20,7 @@
 
 # XXX: Edit resource name
 resource "github_repository_collaborator" "ci" {
-  repository = "my-repo"  # XXX: Edit
+  repository = "my-repo"            # XXX: Edit
   username   = "my-ci-machine-user" # XXX: Edit
   permission = "pull"
 }
