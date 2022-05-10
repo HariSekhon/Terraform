470e2fec11f3b6afc3f5c6322b42e98a6ecc6349
Hari Sekhon <harisekhon@gmail.com>
2022-05-10 10:40:54 +0100

    updated github_file.tf

--- github_file.tf
+++ github_file.tf
@@ -24,8 +24,7 @@ resource "github_repository_file" "MYFILE" {
   repository     = github_repository.foo.name
   branch         = "main" # or "master"
   file           = ".gitignore"
-  content        = "**/*.tfstate"
-  #commit_message = "Managed by Terraform"
+  #content        = "**/*.tfstate"
   # ensure there is a newline at end of file via EOT style so people with IDEs or pre-commit hooks aren't changing the file during PRs
   content             = <<EOF
 # Managed by Terraform - DO NOT EDIT
@@ -34,6 +33,7 @@ EOF
   # requires both or neither - uses the account owning the github token as the author if omitted
   #commit_author = "Terraform"
   #commit_email  = "terraform@MYCOMPANY.COM"
+  #commit_message = "Managed by Terraform"
   overwrite_on_create = false
 
   lifecycle {
