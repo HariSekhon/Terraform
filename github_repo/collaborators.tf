b29efd81a4f8cff57478335a0503084b659ead84
Hari Sekhon <harisekhon@gmail.com>
2022-06-08 01:52:44 +0100

    updated collaborators.tf

--- github_repo/collaborators.tf
+++ github_repo/collaborators.tf
@@ -21,7 +21,8 @@
 resource "github_repository_collaborator" "ci" {
   repository = github_repository.repo.id
   username   = "my-ci-machine-user"
-  permission = var.ci_permission # may need to set this to "admin" if using this account to run Terraform CI/CD
+  #permission = var.ci_permission # may need to set this to "admin" if using this account to run Terraform CI/CD
+  permission = "admin"
 }
 
 resource "github_user_invitation_accepter" "ci" {
