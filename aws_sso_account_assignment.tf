7bd1a9bec6ee98b634bc1b4dbe29d845fd41ec08
Hari Sekhon <harisekhon@gmail.com>
2023-01-25 19:28:31 +0000

    updated aws_sso_account_assignment.tf

--- aws_sso_account_assignment.tf
+++ aws_sso_account_assignment.tf
@@ -25,8 +25,8 @@ locals {
     myproject-aws-auditor                    = ["AWSReadOnlyAccess"]
     myproject-aws-billing                    = ["Billing"]
     myproject-aws-data-developers            = ["DataDevelopers"]
-    myproject-aws-developer   			         = ["AWSPowerUserAccess"]
-    myproject-aws-users 										 = ["AWSReadOnlyAccess"]
+    myproject-aws-developer                  = ["AWSPowerUserAccess"]
+    myproject-aws-users                      = ["AWSReadOnlyAccess"]
   }
 
   group_permset_map_dev_expanded = flatten([
